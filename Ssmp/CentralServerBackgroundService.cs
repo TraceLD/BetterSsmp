2cd3ae414185f85d46a36f8ede163b799cbea997
john01dav <john01dav@gmail.com>
2021-03-04 00:08:42 -0600

    Fixed some DI problems. This would have been caught in testing, but testing phase is shorter than usual to get this code out quicker.

---
+++ Ssmp/CentralServerBackgroundService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using static Ssmp.ConnectedClient;
+
+namespace Ssmp
+{
+    public class CentralServerBackgroundService : BackgroundService
+    {
+        private readonly ILogger<CentralServerBackgroundService> _logger;
+        private Handler _handler;
+        private CentralServerService _centralServerService;
+        private int _messageQueueLimit;
+        private readonly IPAddress _ipAddress;
+        private readonly int _port;
+
+        public CentralServerBackgroundService(CentralServerService centralServerService, ILogger<CentralServerBackgroundService> logger, Handler handler, int messageQueueLimit, IPAddress ipAddress, int port)
+        {
+            _centralServerService = centralServerService;
+            _logger = logger;
+            _handler = handler;
+            _messageQueueLimit = messageQueueLimit;
+            _ipAddress = ipAddress;
+            _port = port;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) //TODO: Don't ignore the stopping token
+        {
+            var tasks = new List<Task>();
+
+            var listener = new TcpListener(_ipAddress, _port);
+            
+            while(!stoppingToken.IsCancellationRequested)
+            {
+                tasks.Add(listener.AcceptTcpClientAsync());
+                var completedTask = await Task.WhenAny(tasks);
+                tasks.Remove(completedTask);
+                if(completedTask is Task<TcpClient> newConnection)
+                {
+                    var client = ConnectedClient.Adopt(_handler, await newConnection, _messageQueueLimit);
+                    _centralServerService._connectedClients = _centralServerService._connectedClients.Add(client);
+                    tasks.Add(client.Spin());
+                }
+                else if(completedTask is Task<ConnectedClient> endedClient)
+                {
+                    _centralServerService._connectedClients = _centralServerService._connectedClients.Remove(await endedClient);
+                }
+                else
+                {
+                    await completedTask;
+                }
+            }
+        }
+
+    }
+}
--- Ssmp/CentralServerService.cs
+++ Ssmp/CentralServerService.cs
@@ -1,64 +1,15 @@
-﻿using System.Collections.Immutable;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using static Ssmp.ConnectedClient;
+using System.Collections.Immutable;
 
-namespace Ssmp
-{
-    public class CentralServerService : BackgroundService
+namespace Ssmp{
+
+    public class CentralServerService
     {
-        private readonly ILogger<CentralServerService> _logger;
-        private Handler _handler;
-        private volatile ImmutableList<ConnectedClient> _connectedClients;
-        private int _messageQueueLimit;
-        private readonly IPAddress _ipAddress;
-        private readonly int _port;
-
-        public CentralServerService(ILogger<CentralServerService> logger, Handler handler, int messageQueueLimit, IPAddress ipAddress, int port)
+        internal volatile ImmutableList<ConnectedClient> _connectedClients = ImmutableList<ConnectedClient>.Empty;
+        public ImmutableList<ConnectedClient> GetConnectedClients()
         {
-            _logger = logger;
-            _handler = handler;
-            _messageQueueLimit = messageQueueLimit;
-            _ipAddress = ipAddress;
-            _port = port;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken) //TODO: Don't ignore the stopping token
-        {
-            var tasks = new List<Task>();
-
-            var listener = new TcpListener(_ipAddress, _port);
-            
-            while(!stoppingToken.IsCancellationRequested)
-            {
-                tasks.Add(listener.AcceptTcpClientAsync());
-                var completedTask = await Task.WhenAny(tasks);
-                tasks.Remove(completedTask);
-                if(completedTask is Task<TcpClient> newConnection)
-                {
-                    var client = ConnectedClient.Adopt(_handler, await newConnection, _messageQueueLimit);
-                    _connectedClients = _connectedClients.Add(client);
-                    tasks.Add(client.Spin());
-                }
-                else if(completedTask is Task<ConnectedClient> endedClient)
-                {
-                    _connectedClients = _connectedClients.Remove(await endedClient);
-                }
-                else
-                {
-                    await completedTask;
-                }
-            }
-        }
-
-        public ImmutableList<ConnectedClient> GetConnectedClients(){
             return _connectedClients;
         }
 
     }
+
 }
