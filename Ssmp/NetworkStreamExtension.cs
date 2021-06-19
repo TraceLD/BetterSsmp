bd26ede5b2b32a03539956d9608cf54a94322b70
TraceLD <lukaszdabrowski881@gmail.com>
2021-06-19 02:10:24 +0200

    fix infinite loop

--- Ssmp/NetworkStreamExtension.cs
+++ Ssmp/NetworkStreamExtension.cs
@@ -5,16 +5,21 @@ namespace Ssmp
 {
     public static class NetworkStreamExtension
     {
-
         public static async Task ReadNBytes(this NetworkStream ns, int n, byte[] buffer)
         {
             var index = 0;
+
             while(index < n)
             {
-                index += await ns.ReadAsync(buffer, index, n-index);
+                var bytes = await ns.ReadAsync(buffer, index, n-index);
+
+                if (bytes <= 0) 
+                {
+                    break;
+                }
+
+                index += bytes;
             }
         }
-
     }
-
 }
