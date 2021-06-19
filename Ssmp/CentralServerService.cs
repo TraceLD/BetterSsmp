f5fa2e781ef3cc6379912a1cc20422a407435fd3
TraceLD <lukaszdabrowski881@gmail.com>
2021-06-19 02:06:47 +0200

    fix formatting

--- Ssmp/CentralServerService.cs
+++ Ssmp/CentralServerService.cs
@@ -3,20 +3,20 @@ using System.Collections.Immutable;
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
+
 using static Ssmp.ConnectedClient;
 
-namespace Ssmp{
-
+namespace Ssmp
+{
     public class CentralServerService
     {
         private readonly Handler _handler;
         private readonly int _messageQueueLimit;
         private readonly List<Task> _tasks = new List<Task>();
         private readonly TcpListener _listener;
-        
+
         private volatile ImmutableList<ConnectedClient> _connectedClients = ImmutableList<ConnectedClient>.Empty;
-        
+
         public ImmutableList<ConnectedClient> ConnectedClients => _connectedClients;
 
         public CentralServerService(Handler handler, int messageQueueLimit, IPAddress ipAddress, int port)
@@ -24,20 +24,26 @@ namespace Ssmp{
             _handler = handler;
             _messageQueueLimit = messageQueueLimit;
             _listener = new TcpListener(ipAddress, port);
+            
             _listener.Start();
         }
 
-        internal async Task SpinOnce(){
+        internal async Task SpinOnce()
+        {
             _tasks.Add(_listener.AcceptTcpClientAsync());
+
             var completedTask = await Task.WhenAny(_tasks);
+
             _tasks.Remove(completedTask);
-            if(completedTask is Task<TcpClient> newConnection)
+
+            if (completedTask is Task<TcpClient> newConnection)
             {
                 var client = ConnectedClient.Adopt(_handler, await newConnection, _messageQueueLimit);
+                
                 _connectedClients = _connectedClients.Add(client);
                 _tasks.Add(client.Spin());
             }
-            else if(completedTask is Task<ConnectedClient> endedClient)
+            else if (completedTask is Task<ConnectedClient> endedClient)
             {
                 _connectedClients = _connectedClients.Remove(await endedClient);
             }
@@ -46,7 +52,5 @@ namespace Ssmp{
                 await completedTask;
             }
         }
-
     }
-
 }
